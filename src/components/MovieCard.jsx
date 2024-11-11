44e5cede3ddf4056d0ce09d32923ff469dc0c53e
yoonhyeonjeong <h_j111@naver.com>
2024-11-11 22:25:37 +0900

    수정

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,10 +2,13 @@ const MovieCard = ({data}) => {
     const baseUrl = "https://image.tmdb.org/t/p/w500";
     return (
         <div>
-            <img
-                src={baseUrl + data.poster_path}
-                alt={data.title}
-            />
+            <div className="w-full h-500 relative">
+                <img
+                    className="w-full h-full object-cover"
+                    src={baseUrl + data.poster_path}
+                    alt={data.title}
+                />
+            </div>
             <p className="text-lg text-center mt-10">{data.title}</p>
             <p className="movie-average">평점 {data.vote_average}</p>
         </div>
--- src/components/MovieLogin.jsx
+++ src/components/MovieLogin.jsx
@@ -1,10 +1,10 @@
 import {useRef, useState} from "react";
-import supabase from "../supabase/supabase"; // Supabase 클라이언트 가져오기
-import {useNavigate} from "react-router-dom"; // 리디렉션을 위한 React Router 사용
+import supabase from "../supabase/supabase";
+import {useNavigate} from "react-router-dom";
 
 const MovieLogin = () => {
-    const emailRef = useRef(null); // 이메일 입력 필드에 대한 ref
-    const passwordRef = useRef(null); // 비밀번호 입력 필드에 대한 ref
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
     const [error, setError] = useState("");
     const navigate = useNavigate();
 
@@ -12,8 +12,8 @@ const MovieLogin = () => {
         e.preventDefault();
         setError("");
 
-        const email = emailRef.current.value; // ref로 이메일 값 가져오기
-        const password = passwordRef.current.value; // ref로 비밀번호 값 가져오기
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
 
         try {
             const {user, session, error} = await supabase.auth.signInWithPassword({
@@ -23,8 +23,7 @@ const MovieLogin = () => {
 
             if (error) throw error;
 
-            // 로그인 성공 후 홈 화면으로 리디렉션
-            navigate("/home"); // 홈 화면 또는 원하는 페이지로 리디렉션
+            navigate("/");
         } catch (err) {
             setError("로그인 실패: " + err.message);
         }
@@ -49,7 +48,7 @@ const MovieLogin = () => {
                             type="email"
                             name="email"
                             id="email"
-                            ref={emailRef} // ref로 이메일 필드에 연결
+                            ref={emailRef}
                             className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                         />
                     </div>
@@ -65,7 +64,7 @@ const MovieLogin = () => {
                             type="password"
                             name="password"
                             id="password"
-                            ref={passwordRef} // ref로 비밀번호 필드에 연결
+                            ref={passwordRef}
                             className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                         />
                     </div>
